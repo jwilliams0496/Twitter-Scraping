abbfcbcdb43e68535cc1cc3b36770a7eb3aa6003
skullblend <hd38933@umbc.edu>
2017-03-21 16:22:39 -0400

    Second commit, data can now be written to a file a read from. Vectorizing the files for sk learn will be the next step

--- twitterScrape.py
+++ twitterScrape.py
@@ -1,59 +1,97 @@
-# File: twitterScrape.py
-# Author: James Williams
-# Date: 3/10/17
-
 from twython import Twython
+import time
 
-user_name = "JonTronShow"
-hash_tag = "jontronshow"
-search_type = 0 #0 for tweets from a hashtag, 1 for tweets from a user
-search_size = 100
+username = "JonTronShow"
+hashtag = "jontronshow"
+searchType = 1
+searchSize = 100
 
 def main():
-    f = open("APIKeys.txt" , "r")
+    tweetData = readDataFile("dataFile.txt")
 
-    access_token = f.readline()
-    access_token = access_token.strip('\n')
+    print(tweetData)
+    print(len(tweetData))
     
-    access_token_secret = f.readline()
-    access_token_secret = access_token_secret.strip('\n')
-    
-    consumer_key = f.readline()
-    consumer_key = consumer_key.strip('\n')
-    
-    consumer_secret = f.readline()
-    consumer_secret  = consumer_secret.strip('\n')
+def vectorizeData(words):
+    pass
 
-    print(access_token)
-    print(access_token_secret)
-    print(consumer_key)
-    print(consumer_secret)
+def readDataFile(fileName):
+    dataFile = open(fileName, 'r')
+    return dataFile.read()
+    
+def getData():
+    accessToken = getKeys()[0]
+    accessTokenSecret = getKeys()[1]
+    consumerKey = getKeys()[2]
+    consumerSecret = getKeys()[3]
 
+    print(accessToken)
+    print(accessTokenSecret)
+    print(consumerKey)
+    print(consumerSecret)
+    
     print("Obtaining OAuth 2 token\n\n")
 
-    twitter = Twython(consumer_key, consumer_secret, oauth_version=2)
-    ACCESS_TOKEN = twitter.obtain_access_token()
+    twitter = Twython(consumerKey, consumerSecret, oauth_version=2)
+    ACCESS_TOKEN = twitter.obtain_accessToken()
 
     print("Access token granted\n")
 
-    twitter = Twython(consumer_key, access_token=ACCESS_TOKEN)
+    twitter = Twython(consumerKey, accessToken=ACCESS_TOKEN)
 
-    if (search_type == 1):
-        user_timeline = twitter.get_user_timeline(screen_name=user_name)
+    if (searchType == 0):
+        userTimeline = twitter.get_userTimeline(screen_name = username)
+        
+        print("\n\nTweets from ", username, "\n\n")
         
         # And print the tweets for that user.
-        for tweet in user_timeline:
+        for tweet in userTimeline:
             print(tweet['text'])
+            
+        user_retweets = twitter.get_retweets(screen_name = username)
+            
+        print("\n\nRe-Tweets from ", username, "\n\n")
 
+        for retweet in user_retweets:
+            print(retweet['text'])
         
-    if (search_type == 0):
+    if (searchType == 1):
         print("Searching for tweets\n")
-        search = twitter.search(q=hash_tag, count=search_size)
+        
+        tweetIds = {}
+
+        search = twitter.search(q=hashtag, count=searchSize)
         tweets = search['statuses']
 
         for tweet in tweets:
             print(tweet['id_str'], '\n', tweet['text'], '\n\n\n')
   
-            print("\n\n\n*********", len(tweets), "**********\n\n\n")
+            if (tweet['id_str'] not in tweetIds):
             
+            # Value is arbitrary, key needed to make sure no duplicates are written to file
+            tweetIds[tweet['id_str']] = 1
+
+            dataFile = open('dataFile.txt', 'a+')
+            dataFile.write("\t" + formatUnicode(tweet['text'] + "\n\n"))
+            
+def getKeys():
+    file = open("APIKeys.txt" , "r")
+    
+    accessToken = file.readline()
+    accessToken = accessToken.strip('\n')
+    
+    accessTokenSecret = file.readline()
+    accessTokenSecret = accessTokenSecret.strip('\n')
+    
+    consumerKey = file.readline()
+    consumerKey = consumerKey.strip('\n')
+    
+    consumerSecret = file.readline()
+    consumerSecret  = consumerSecret.strip('\n')
+    
+    return accessToken, accessTokenSecret, consumerKey, consumerSecret
+
+def formatUnicode(text):
+    return ''.join([i if ord(i) < 128 else ' ' for i in text])    
+
 main()
